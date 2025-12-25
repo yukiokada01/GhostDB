5f83d11141946f04aa048505b1cc304cf206de57
yukiokada01 <xwcviqh67424@outlook.com>
2025-12-25 16:52:58 +0800

    Switch deployment to GhostDocumentRegistry

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedRegistry = await deploy("GhostDocumentRegistry", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`GhostDocumentRegistry contract: `, deployedRegistry.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_ghost_document_registry"; // id required to prevent reexecution
+func.tags = ["GhostDocumentRegistry"];
